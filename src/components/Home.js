de68572eab413ed1d6d4e29d1dee6f174b27c4f4
“TJ <tylerchecketts@gmail.com>
2018-10-08 21:51:43 -0600

    add picture to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import { Header, Image } from 'semantic-ui-react';
+import ProPic from '../images/pro-pic-reduced.jpg';
 
 class Home extends Component {
   render() {
@@ -8,6 +9,7 @@ class Home extends Component {
         <Header as='h1' textAlign='center'>
           TJ Checketts
         </Header>
+        <Image src={ProPic} size='medium' circular centered />
         <Header as='h2' textAlign='center'>
           Software Developer
         </Header>
